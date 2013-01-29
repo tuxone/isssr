006154ca5c1d481c8348b3234cd709c03e5a07d5
Alessandro <cyberalexgm@gmail.com>
2013-01-29 10:42:36 +0100

    first test (tag)

--- src/Isssr/CoreBundle/Controller/DefaultController.php
+++ src/Isssr/CoreBundle/Controller/DefaultController.php
@@ -1,17 +1,18 @@
 <?php
 
 namespace Isssr\CoreBundle\Controller;
-
 use Symfony\Component\Security\Core\SecurityContext;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+ 
+class DefaultController extends Controller {
+	public function indexAction() {
+		$scontext = $this->container->get('security.context');
+		$token = $scontext->getToken();
+		$user = $token->getUser();
 
-class DefaultController extends Controller
-{	
-	public function indexAction()
-    {
-    	$user = $this->container->get('security.context')->getToken()->getUser();
-    	
-        return $this->render('IsssrCoreBundle:Default:index.html.twig', array('name' => "ciao", 'user' => $user));
-    }
+		return $this
+				->render('IsssrCoreBundle:Default:index.html.twig',
+						array('name' => "ciao", 'user' => $user));
+	}
 }
---
+++ src/Isssr/CoreBundle/Tests/Entity/TagTest.php
@@ -0,0 +1,53 @@
+<?php
+namespace Isssr\CoreBundle\Tests\Utility;
+
+use Isssr\CoreBundle\Entity\Tag;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TagTest extends WebTestCase
+{
+
+	private $em;	
+	private $TAGTITLE = "TAG_TITLE_TEST";
+	
+	public function setUp()
+	{
+		static::$kernel = static::createKernel();
+		static::$kernel->boot();
+		$this->em = static::$kernel->getContainer()
+		->get('doctrine')
+		->getEntityManager()
+		;		
+	}
+	
+    public function testTag()
+    {
+    	$tag = new Tag();
+		$tag->setTitle($this->$TAGTITLE);
+		
+		// Test su metodi costruttore get & set
+        $this->assertEquals($this->$TAGTITLE, $tag->getTitle());
+        
+        // Test add
+        $this->em->persist($tag);
+        $this->em->flush();
+        $tmptag = $this->em->getRepository('IsssrCoreBundle:Tag')->find($tag->getId());
+        $this->assertEquals($tmptag, $tag);
+        
+        // Test Unique on 'title'
+        
+        // Test remove
+        $id = $tag->getId();
+        $this->em->remove($tag);
+        $this->em->flush();
+        $tmptag = $this->em->getRepository('IsssrCoreBundle:Tag')->find($id);
+        $this->assertNull($tmptag);
+    }
+    
+    protected function tearDown()
+    {
+    	parent::tearDown();
+    	$this->em->close();
+    }
+}
