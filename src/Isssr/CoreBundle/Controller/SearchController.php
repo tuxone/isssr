346568cbe7bbfd5b5d62308884561489a61b7c50
Angelo Di Stella <angelo.distella@gmail.com>
2013-02-11 10:56:14 +0100

    search

---
+++ src/Isssr/CoreBundle/Controller/SearchController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Isssr\CoreBundle\Controller;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Isssr\CoreBundle\Entity\Search;
+use Isssr\CoreBundle\Form\SearchType;
+
+
+/**
+ * Search controller.
+ *
+ */
+class SearchController extends Controller
+{
+	public function searchAction()
+	{
+		$scontext = $this->container->get('security.context');
+		$token = $scontext->getToken();
+		$user = $token->getUser();
+		
+		$entity = new Search();
+        $form   = $this->createForm(new SearchType(), $entity);
+
+              
+        return $this->render('IsssrCoreBundle:Search:search.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        	'user' => $user,
+        ));
+	}
+	
+	public function resultAction(Request $request)
+	{
+		$scontext = $this->container->get('security.context');
+		$token = $scontext->getToken();
+		$user = $token->getUser();
+		
+	
+		$em = $this->getDoctrine()->getManager();
+		
+		$entity  = new Search();
+		$form = $this->createForm(new SearchType(), $entity);
+		$form->bind($request);
+		
+		$goal = $em->getRepository('IsssrCoreBundle:Goal')->find($entity->getId());
+		
+		$goals = array();
+		$goals[] = $goal;
+		
+		if (!$goals) {
+			throw $this->createNotFoundException('Unable to find Goal entities.');
+		}
+		return $this->render('IsssrCoreBundle:Search:result.html.twig', array(
+				'goals'      => $goals,
+				'user' => $user,
+		));
+	}	
+}
---
+++ src/Isssr/CoreBundle/Form/SearchType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Isssr\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class SearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        	->add('id')
+            ->add('title')
+            ->add('description')
+            ->add('tags', null, array('label' => 'Tags'))
+//             ->add('goalOwner', null, array('label' => 'Owner *'))
+//             ->add('goalEnactor', null, array('label' => 'Enactor *'))
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Isssr\CoreBundle\Entity\Search'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'isssr_corebundle_searchtype';
+    }
+}
