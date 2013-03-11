b515e6ee0bbe8be88500e404a1312a28480e6f1c
Angelo Di Stella <angelo.distella@gmail.com>
2013-03-11 19:42:21 +0100

    re-working-qualsiasi2

--- src/Isssr/CoreBundle/Services/NotifierManager.php
+++ src/Isssr/CoreBundle/Services/NotifierManager.php
@@ -8,7 +8,6 @@ use Isssr\CoreBundle\Entity\Goal;
 
 use Doctrine\ORM\EntityManager;
 use Doctrine\Common\Collections\ArrayCollection;
-use Isssr\CoreBundle\Entity\UserInGoal;
 
 class NotifierManager
 {
@@ -24,13 +23,18 @@ class NotifierManager
 		$goal = $role->getGoal();
 		if ($role->getRole() == UserInGoal::ROLE_SUPER){
 			if ($role->getStatus() == UserInGoal::STATUS_FIRST_VALIDATION_NEEDED) $body = $this->bodySuperFirstSent($goal);
-			else $body = $this->bodySuperOtherSent($goal);
+			else if ($role->getStatus() == UserInGoal::STATUS_VALIDATION_NEEDED) $body = $this->bodySuperOtherSent($goal);
+			else if ($role->getStatus() == UserInGoal::STATUS_GOAL_ACCEPTED) $body = $this->bodySyperAccept($user, $goal);
+			else if ($role->getStatus() == UserInGoal::STATUS_GOAL_REJECTED) $body = $this->bodySuperReject($user, $goal);
 		}
 		else if ($role->getRole() == UserInGoal::ROLE_ENACTOR) {
-			$body = $this->bodyEnactor($goal);	
+			if ($role->getStatus() == UserInGoal::STATUS_VALIDATION_NEEDED) $body = $this->bodyEnactor($goal);	
+			else if ($role->getStatus() == UserInGoal::STATUS_GOAL_ASSIGNED) $body = $this->bodyEnactorAccept($user, $goal);
+			else if ($role->getStatus() == UserInGoal::STATUS_GOAL_REJECTED) $body = $this->bodyEnactorReject($user, $goal);
+			
 		}
 		else if ($role->getRole() == UserInGoal::ROLE_QS) {
-			$body = $this->bodyQs($goal)
+			$body = $this->bodyQs($goal);
 		}
 		else if($role->getRole() == UserInGoal::ROLE_MMDM) {
 			$body = $this->bodyMmdm($goal);
@@ -56,10 +60,30 @@ class NotifierManager
     {
     	return 'The Goal '.$goal->getTitle().', which some super owner previously refused, has been modified, Validate it agan, please';
     }
+    
+    private function bodySyperAccept(User $user, Goal $goal)
+    {
+    	return 'The Goal Super Owner '.$user->getUsername().' did accept the goal '.$goal->getTitle();	
+    }
+    
+    private function bodySuperReject(User $user, Goal $goal)
+    {
+    	return 'The Goal Super Owner '.$user->getUsername().' did reject the goal '.$goal->getTitle();
+    }
 	
     private function bodyEnactor(Goal $goal)
     {
     	return "The Goal owner ".$goal->getOwner()." selected you as the Enactor for the Goal ".$goal->getTitle().".";
+    }
+    
+    private function bodyEnactorAccept(User $user, Goal $goal)
+    {
+    	return 'The proposed Goal Enactor '.$goal->getEnactor()->getUsername().' for the Goal '.$goal->getTitle().' did accept your proposal.';
+    }
+    
+    private function bodyEnactorReject(User $user, Goal $goal)
+    {
+    	 return 'The proposed Goal Enactor '.$goal->getEnactor()->getUsername().' for the Goal '.$goal->getTitle().' did reject your proposal.';
     }
     
     private function bodyMmdm(Goal $goal)
