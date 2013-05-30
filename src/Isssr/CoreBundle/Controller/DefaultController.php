890fba59d53249bdbbc98d1f825da67bfe282f8d
Alessandro <cyberalexgm@gmail.com>
2013-05-30 18:01:28 +0200

    update homepage

--- src/Isssr/CoreBundle/Controller/DefaultController.php
+++ src/Isssr/CoreBundle/Controller/DefaultController.php
@@ -7,15 +7,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  
 class DefaultController extends Controller {
 	public function indexAction() {
-		$scontext = $this->container->get('security.context');
-		$token = $scontext->getToken();
-		$user = $token->getUser();
+
+		$user = $this->getUser();
 
         $em = $this->getDoctrine()->getManager();
+        $gm = $this->get('isssr_core.goalmanager');
+
+        $users = $em->getRepository('IsssrCoreBundle:User')->findAll();
+
+        $grids = $em->getRepository('IsssrCoreBundle:Grid')->findAll();
 
         $goals = $em->getRepository('IsssrCoreBundle:Goal')->findAll();
-
-        $gm = $this->get('isssr_core.goalmanager');
 
         $reached = 0;
 
@@ -25,15 +27,18 @@ class DefaultController extends Controller {
                 $reached++;
         }
 
+        $avggoalspergrid = round(count($goals) / count($grids), 2);
 
 
 		return $this
 				->render('IsssrCoreBundle:Default:index.html.twig',
 						array(
-                            'name' => "ciao",
                             'user' => $user,
+                            'nusers' => count($users),
+                            'ngrids' => count($grids),
                             'ngoals' => count($goals),
                             'nreached' => $reached,
+                            'avggoalspergrid' => $avggoalspergrid,
                         )
             );
 	}
